42abc688f42147bfe37d652ade071aa4ee775740
Pavel Grivachev <pavel@fivetran.com>
2021-03-29 15:33:59 +0300

    feature(client, server): initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# dbt-extension README
+
+The repository contains dbt extension for Visual Studio Code.
+
+## How to build
+
+To build extension locally you need to have `npm` installed.
+
+Run the following commands in the root directory
+
+```bash
+$ npm install
+$ npm run compile
+$ code .
+```
+
+The above installs all dependencies and opens the project containing both the client and server code.
+
---
+++ server/src/server.ts
@@ -0,0 +1,107 @@
+import {
+	createConnection,
+	TextDocuments,
+	ProposedFeatures,
+	InitializeParams,
+	DidChangeConfigurationNotification,
+	CompletionItem,
+	CompletionItemKind,
+	TextDocumentPositionParams,
+	TextDocumentSyncKind,
+	InitializeResult,
+	Hover,
+	HoverParams,
+	Range
+} from 'vscode-languageserver/node';
+
+import {
+	TextDocument
+} from 'vscode-languageserver-textdocument';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+let connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability: boolean = false;
+
+connection.onInitialize((params: InitializeParams) => {
+	let capabilities = params.capabilities;
+
+	// Does the client support the `workspace/configuration` request?
+	// If not, we fall back using global settings.
+	hasConfigurationCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.configuration
+	);
+
+	const result: InitializeResult = {
+		capabilities: {
+			textDocumentSync: TextDocumentSyncKind.Incremental,
+			completionProvider: {
+				resolveProvider: true
+			},
+			hoverProvider : true,
+		}
+	};
+	return result;
+});
+
+connection.onInitialized(() => {
+	if (hasConfigurationCapability) {
+		// Register for all configuration changes.
+		connection.client.register(DidChangeConfigurationNotification.type, undefined);
+	}
+});
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+		// The pass parameter contains the position of the text document in
+		// which code complete got requested. For the example we ignore this
+		// info and always provide the same completion items.
+		return [
+			{
+				label: 'TypeScript',
+				kind: CompletionItemKind.Text,
+				data: 1
+			},
+			{
+				label: 'JavaScript',
+				kind: CompletionItemKind.Text,
+				data: 2
+			}
+		];
+	}
+);
+
+connection.onHover(
+	(hoverParams: HoverParams): Hover => {
+		let text = documents.get(hoverParams.textDocument.uri)?.getText(Range.create(hoverParams.position.line, hoverParams.position.character, hoverParams.position.line, hoverParams.position.character + 1));
+		return {
+			contents: {
+				kind: 'markdown',
+				value: `This is hover for \`${text}\``
+			}
+		}
+	}
+);
+
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve(
+	(item: CompletionItem): CompletionItem => {
+		if (item.data === 1) {
+			item.detail = 'TypeScript details';
+			item.documentation = 'TypeScript documentation';
+		} else if (item.data === 2) {
+			item.detail = 'JavaScript details';
+			item.documentation = 'JavaScript documentation';
+		}
+		return item;
+	}
+);
+
+documents.listen(connection);
+connection.listen();
