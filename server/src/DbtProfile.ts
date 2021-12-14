5cb01f392db7f9e84e3123ea5aff330ffbf131da
Dmitry Boykov <dmitry.boykov@fivetran.com>
2021-12-15 00:27:17 +0300

    Annotate validation method

--- server/src/DbtProfile.ts
+++ server/src/DbtProfile.ts
@@ -21,6 +21,11 @@ export abstract class Client {}
 export interface DbtProfile {
   getDocsUrl(): string;
   getData(profile: any): ProfileData;
+  /**
+   * Validate dbt profile according to specified type and authentication method
+   * @param targetConfig target config specified in profiles.yml
+   * @returns error message or undefined if profile is valid
+   */
   validateProfile(targetConfig: any): string | undefined;
   createClient(data: ProfileData): Client;
   authenticateClient(client: Client): Promise<string | undefined>;
