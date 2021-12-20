66457d7f07f56713471d95e5821ac7e723f6875f
Dmitry Boykov <dmitry.boykov@fivetran.com>
2021-12-20 15:46:02 +0300

    Fix file watcher pattern

--- client/src/DbtLanguageClient.ts
+++ client/src/DbtLanguageClient.ts
@@ -32,7 +32,7 @@ export class DbtLanguageClient implements Disposable {
     const clientOptions = {
       documentSelector: SUPPORTED_LANG_IDS.map(langId => ({ scheme: 'file', language: langId, pattern: `${dbtProjectUri.fsPath}/**/*` })),
       synchronize: {
-        fileEvents: [workspace.createFileSystemWatcher('./dbt_project.yml'), workspace.createFileSystemWatcher('**/manifest.json')],
+        fileEvents: [workspace.createFileSystemWatcher('**/dbt_project.yml'), workspace.createFileSystemWatcher('**/manifest.json')],
       },
       outputChannel: outputChannel,
       workspaceFolder: <WorkspaceFolder>{ uri: dbtProjectUri },
