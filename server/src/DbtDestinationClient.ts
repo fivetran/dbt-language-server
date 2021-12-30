d4f12d3df5c66027d5a93f8060021308f7f46040
Dmitry Boykov <dmitry.boykov@fivetran.com>
2021-12-30 10:09:16 +0300

    Remove extra line from method description

--- server/src/DbtDestinationClient.ts
+++ server/src/DbtDestinationClient.ts
@@ -1,7 +1,6 @@
 export interface DbtDestinationClient {
   /**
    * Tests connection to destination
-   * @param client dbt profile client
    * @returns undefined in case of connection successful and error string otherwise
    */
   test(): Promise<string | undefined>;
