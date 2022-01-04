9a4310202449a24041711f68215b31f3af33c4db
fivetran-dmitryboykov <93979055+fivetran-dmitryboykov@users.noreply.github.com>
2022-01-05 00:46:43 +0300

    Jinja completions testing (#20)
    * Use separate dbt project for jinja completion testing
    
    * Add completion-jinja project to the e2e workspace
    
    * Add new models for completion testing
    
    * Use predefined list of labels to test completions

--- e2e/src/completion_jinja.spec.ts
+++ e2e/src/completion_jinja.spec.ts
@@ -1,53 +1,35 @@
 import * as assert from 'assert';
-import * as fs from 'fs';
 import * as vscode from 'vscode';
 import { CompletionItem } from 'vscode';
-import { activateAndWait, getDocPath, getDocUri, getManifestModels, setTestContent, testCompletion, triggerCompletion } from './helper';
+import { activateAndWait, getCustomDocUri, setTestContent, testCompletion, triggerCompletion } from './helper';
 
 suite('Should do completion inside jinjas expression', () => {
-  const FILE_NAME = 'completion_jinja.sql';
-
-  let models: string[];
-  let completionJinjaContent: string;
-
-  suiteSetup(function () {
-    completionJinjaContent = fs.readFileSync(getDocPath(FILE_NAME)).toString();
-  });
-
-  suiteTeardown(async function () {
-    await setTestContent(completionJinjaContent);
-  });
+  const PROJECT_FILE_NAME = 'completion-jinja/models/completion_jinja.sql';
 
   test('Should suggest models for ref function by pressing "("', async () => {
-    const docUri = getDocUri(FILE_NAME);
+    const docUri = getCustomDocUri(PROJECT_FILE_NAME);
     await activateAndWait(docUri);
     await setTestContent('select * from {{ref(');
-
-    models = getManifestModels();
     await testCompletion(docUri, new vscode.Position(0, 20), getCompletionList(true), '(');
   });
 
   test('Should suggest models for ref function', async () => {
-    const docUri = getDocUri(FILE_NAME);
+    const docUri = getCustomDocUri(PROJECT_FILE_NAME);
     await activateAndWait(docUri);
     await setTestContent('select * from {{ref(');
-
-    models = getManifestModels();
     await testCompletion(docUri, new vscode.Position(0, 20), getCompletionList(true));
   });
 
   test('Should suggest models for ref function by pressing "\'"', async () => {
-    const docUri = getDocUri(FILE_NAME);
+    const docUri = getCustomDocUri(PROJECT_FILE_NAME);
     await activateAndWait(docUri);
     await setTestContent(`select * from {{ref('`);
-
-    models = getManifestModels();
     await testCompletion(docUri, new vscode.Position(0, 21), getCompletionList(false), "'");
   });
 
   test('Should not suggest models outside jinja', async () => {
     // arrange
-    const docUri = getDocUri(FILE_NAME);
+    const docUri = getCustomDocUri(PROJECT_FILE_NAME);
     await activateAndWait(docUri);
     await setTestContent(`select * from {{}}ref('`);
 
@@ -56,13 +38,15 @@ suite('Should do completion inside jinjas expression', () => {
 
     // assert
     const actualLabels = actualCompletionList.items.map(i => <string>i.label);
-
-    models = getManifestModels();
     getCompletionList(false).items.forEach(i => assert.ok(!actualLabels.includes(<string>i.label)));
     getCompletionList(true).items.forEach(i => assert.ok(!actualLabels.includes(<string>i.label)));
   });
 
   function getCompletionList(withQuotes: boolean): { items: vscode.CompletionItem[] } {
-    return { items: models.map(l => <CompletionItem>{ label: withQuotes ? `'${l}'` : l, kind: vscode.CompletionItemKind.Value }) };
+    return { items: getLabels().map(l => <CompletionItem>{ label: withQuotes ? `'${l}'` : l, kind: vscode.CompletionItemKind.Value }) };
+  }
+
+  function getLabels(): string[] {
+    return ['completion_jinja', 'test_table1', 'users'];
   }
 });
