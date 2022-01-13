def88fc03db1de6f6938f4484067ec02aac18079
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-01-13 14:28:51 +0300

    Modify test

--- server/src/test/JinjaParser.spec.ts
+++ server/src/test/JinjaParser.spec.ts
@@ -5,7 +5,7 @@ import { TextDocument } from 'vscode-languageserver-textdocument';
 import { JinjaParser } from '../JinjaParser';
 
 describe('JinjaParser', () => {
-  function shouldFindAllJinjas(fileName: string, ranges: Range[]): void {
+  function shouldFindAllJinjaRanges(fileName: string, ranges: Range[]): void {
     // arrange
     const doc = TextDocument.create('uri', 'id', 1, fs.readFileSync(`${__dirname}/../../src/test/sql_files/${fileName}.sql`, 'utf8'));
 
@@ -13,22 +13,29 @@ describe('JinjaParser', () => {
     const jinjas = new JinjaParser().findAllJinjaRanges(doc);
 
     // assert
-    assert.strictEqual(jinjas?.length, ranges.length);
+    assert.strictEqual(jinjas.length, ranges.length);
     assert.deepStrictEqual(jinjas, ranges);
   }
 
   it('findAllJinjas_shouldFindAllJinjas', () => {
-    shouldFindAllJinjas('simple_query', []);
-    shouldFindAllJinjas('without_expressions', []);
-    shouldFindAllJinjas('one_at_the_beginning', [Range.create(0, 0, 7, 2)]);
-    shouldFindAllJinjas('one_at_the_end', [Range.create(8, 0, 8, 27)]);
-    shouldFindAllJinjas('four', [Range.create(0, 0, 0, 54), Range.create(1, 5, 1, 22), Range.create(1, 43, 1, 58), Range.create(2, 0, 2, 14)]);
-    shouldFindAllJinjas('five', [
+    shouldFindAllJinjaRanges('simple_query', []);
+    shouldFindAllJinjaRanges('without_expressions', []);
+    shouldFindAllJinjaRanges('one_at_the_beginning', [Range.create(0, 0, 7, 2)]);
+    shouldFindAllJinjaRanges('one_at_the_end', [Range.create(8, 0, 8, 27)]);
+    shouldFindAllJinjaRanges('four', [
+      Range.create(0, 0, 0, 54),
+      Range.create(1, 5, 1, 22),
+      Range.create(1, 43, 1, 58),
+      Range.create(2, 0, 2, 14),
+      Range.create(0, 0, 2, 14),
+    ]);
+    shouldFindAllJinjaRanges('five', [
       Range.create(0, 0, 0, 73),
       Range.create(4, 4, 4, 47),
       Range.create(5, 36, 5, 54),
       Range.create(5, 76, 5, 94),
       Range.create(6, 4, 6, 16),
+      Range.create(4, 4, 6, 16),
     ]);
   });
 });
