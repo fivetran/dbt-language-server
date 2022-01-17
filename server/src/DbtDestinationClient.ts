08dea3495b4bfbed0d67aec98bb97e754582d7e6
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-01-17 13:08:55 +0300

    Rename description of method

--- server/src/DbtDestinationClient.ts
+++ server/src/DbtDestinationClient.ts
@@ -3,7 +3,7 @@ import { Result } from 'neverthrow';
 export interface DbtDestinationClient {
   /**
    * Tests connection to destination
-   * @returns empty in case of connection successful and error string otherwise
+   * @returns Ok in case of connection successful and Err otherwise
    */
   test(): Promise<Result<void, string>>;
 }
