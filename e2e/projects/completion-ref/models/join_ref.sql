c1ed390e9254364bc5f7da44beb20ff5c4c4e300
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-01-18 17:33:41 +0300

    Add test for completions testing inside join expression

--- e2e/projects/completion-ref/models/join_ref.sql
+++ e2e/projects/completion-ref/models/join_ref.sql
@@ -5,5 +5,5 @@
   ) 
 }}
 
-select * from {{ ref('users') }} u
+select u.id from {{ ref('users') }} u
 inner join {{ ref('test_table1') }} t1 on u.name = t1.name
--- e2e/src/completion_ref.spec.ts
+++ e2e/src/completion_ref.spec.ts
@@ -1,19 +1,42 @@
 import * as vscode from 'vscode';
-import { activateAndWait, getCustomDocUri, replaceText, testCompletion } from './helper';
+import { activateAndWait, getCustomDocUri, testCompletion } from './helper';
 
 suite('Should suggest completions after ref aliases', () => {
   const PROJECT_FILE_NAME = 'completion-ref/models/join_ref.sql';
 
-  test('Should suggest columns for ref alias after press .', async () => {
+  test('Should suggest columns for ref alias after press . in select', async () => {
     // arrange
     const docUri = getCustomDocUri(PROJECT_FILE_NAME);
     await activateAndWait(docUri);
-    await replaceText('*', 'u');
 
     // act
     await testCompletion(
       docUri,
       new vscode.Position(7, 9),
+      {
+        items: [
+          { label: 'division', kind: vscode.CompletionItemKind.Value },
+          { label: 'email', kind: vscode.CompletionItemKind.Value },
+          { label: 'id', kind: vscode.CompletionItemKind.Value },
+          { label: 'name', kind: vscode.CompletionItemKind.Value },
+          { label: 'phone', kind: vscode.CompletionItemKind.Value },
+          { label: 'profile_id', kind: vscode.CompletionItemKind.Value },
+          { label: 'role', kind: vscode.CompletionItemKind.Value },
+        ],
+      },
+      '.',
+    );
+  });
+
+  test('Should suggest columns for ref alias after press . in join', async () => {
+    // arrange
+    const docUri = getCustomDocUri(PROJECT_FILE_NAME);
+    await activateAndWait(docUri);
+
+    // act
+    await testCompletion(
+      docUri,
+      new vscode.Position(8, 44),
       {
         items: [
           { label: 'division', kind: vscode.CompletionItemKind.Value },
