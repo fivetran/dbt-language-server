5966d52a4ca4b8c493591276c39d0ba597e64437
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-01-31 16:50:34 +0300

    Fix ref definitions for refs without package

--- server/src/definition/ModelDefinitionFinder.ts
+++ server/src/definition/ModelDefinitionFinder.ts
@@ -36,43 +36,43 @@ export class ModelDefinitionFinder {
       }
 
       const isPackageSpecified = matches.length === 2;
-      const dbPackage = isPackageSpecified ? matches[0].text : projectName;
+      const dbtPackage = isPackageSpecified ? matches[0].text : `'${projectName}'`;
       const model = isPackageSpecified ? matches[1].text : matches[0].text;
 
       const packageSelectionRange = isPackageSpecified
         ? Range.create(
-            document.positionAt(document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index),
+            document.positionAt(document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index + 1),
             document.positionAt(
-              document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index + matches[0].text.length,
+              document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index + matches[0].text.length - 1,
             ),
           )
         : undefined;
       const modelSelectionRange = isPackageSpecified
         ? Range.create(
-            document.positionAt(document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[1].index),
+            document.positionAt(document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[1].index + 1),
             document.positionAt(
-              document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[1].index + matches[1].text.length,
+              document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[1].index + matches[1].text.length - 1,
             ),
           )
         : Range.create(
-            document.positionAt(document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index),
+            document.positionAt(document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index + 1),
             document.positionAt(
-              document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index + matches[0].text.length,
+              document.offsetAt(getAbsolutePosition(expression.range.start, wordRange.start)) + matches[0].index + matches[0].text.length - 1,
             ),
           );
 
       if (packageSelectionRange && positionInRange(position, packageSelectionRange)) {
-        return this.searchPackageDefinition(dbPackage, dbtModels, packageSelectionRange);
+        return this.searchPackageDefinition(dbtPackage, dbtModels, packageSelectionRange);
       } else if (positionInRange(position, modelSelectionRange)) {
-        return this.searchModelDefinition(dbPackage, model, dbtModels, modelSelectionRange);
+        return this.searchModelDefinition(dbtPackage, model, dbtModels, modelSelectionRange);
       }
     }
 
     return undefined;
   }
 
-  searchPackageDefinition(dbPackage: string, dbtModels: ManifestModel[], packageSelectionRange: Range): DefinitionLink[] | undefined {
-    const modelIdPattern = `model.${dbPackage.substring(1).slice(0, -1)}.`;
+  searchPackageDefinition(dbtPackage: string, dbtModels: ManifestModel[], packageSelectionRange: Range): DefinitionLink[] | undefined {
+    const modelIdPattern = `model.${dbtPackage.substring(1).slice(0, -1)}.`;
     return dbtModels
       .filter(m => m.uniqueId.startsWith(modelIdPattern))
       .map(m =>
