194b75aaa2d4e771807ff0d90f5bc8801a1ae286
Pavel Grivachev <pavel@fivetran.com>
2022-02-01 17:57:55 +0300

    add library to check size on each PR (#41)

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(dirname "$0")
 
 cd ${SCRIPT_DIR}/.. # Go to the root (dbt-language-server) folder
 npm install
-npm run compile
+npm run build
 yes | npx vsce package -o e2e-tests.vsix --baseImagesUrl=https://storage.googleapis.com/dbt-ls-images
 yes | unzip e2e-tests.vsix -d e2e-tests
 node e2e/out/runTest $(pwd)/e2e-tests/extension
