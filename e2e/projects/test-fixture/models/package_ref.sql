f2968242b2622ffcdfcb5856b66a4b2e0b05f1eb
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-02-01 17:43:07 +0300

    Add tests for macro definitions

--- e2e/projects/test-fixture/models/package_ref.sql
+++ e2e/projects/test-fixture/models/package_ref.sql
@@ -1,2 +1,6 @@
-select * from dbt_ls_e2e_dataset.users u
+select
+    u.id,
+    {{ extract_first_name(u.name) }},
+    {{ extract_last_name(u.name) }}
+from dbt_ls_e2e_dataset.users u
 inner join {{ ref('my_new_project', 'table_exists') }} as s on u.id = s.id;
--- e2e/src/definition.spec.ts
+++ e2e/src/definition.spec.ts
@@ -23,19 +23,42 @@ suite('ref definitions', () => {
     await activateAndWait(docUri);
 
     // act
-    const packageDefinitions = await triggerDefinition(docUri, new Position(1, 24));
-    const modelDefinitions = await triggerDefinition(docUri, new Position(1, 42));
+    const packageDefinitions = await triggerDefinition(docUri, new Position(5, 24));
+    const modelDefinitions = await triggerDefinition(docUri, new Position(5, 42));
 
     // assert
     assert.ok(packageDefinitions.length > 1);
-    assert.deepStrictEqual(packageDefinitions[0].originSelectionRange, new Range(new Position(1, 19), new Position(1, 33)));
+    assert.deepStrictEqual(packageDefinitions[0].originSelectionRange, new Range(new Position(5, 19), new Position(5, 33)));
 
     assert.strictEqual(modelDefinitions.length, 1);
     assert.ok(modelDefinitions[0].targetUri.path.endsWith('/test-fixture/models/table_exists.sql'));
-    assert.deepStrictEqual(modelDefinitions[0].originSelectionRange, new Range(new Position(1, 37), new Position(1, 49)));
+    assert.deepStrictEqual(modelDefinitions[0].originSelectionRange, new Range(new Position(5, 37), new Position(5, 49)));
   });
 });
 
-// suite('macro definitions', () => {});
+suite('macro definitions', () => {
+  test('Should suggest definitions for macros', async () => {
+    // arrange
+    const docUri = getDocUri('package_ref.sql');
+    await activateAndWait(docUri);
+
+    // act
+    const extractFirstNameDefinitions = await triggerDefinition(docUri, new Position(2, 9));
+    const extractLastNameDefinitions = await triggerDefinition(docUri, new Position(3, 9));
+
+    // assert
+    assert.strictEqual(extractFirstNameDefinitions.length, 1);
+    assert.ok(extractFirstNameDefinitions[0].targetUri.path.endsWith('/test-fixture/macros/name_utils.sql'));
+    assert.deepStrictEqual(extractFirstNameDefinitions[0].originSelectionRange, new Range(new Position(2, 7), new Position(2, 25)));
+    assert.deepStrictEqual(extractFirstNameDefinitions[0].targetSelectionRange, new Range(new Position(0, 9), new Position(0, 27)));
+    assert.deepStrictEqual(extractFirstNameDefinitions[0].targetRange, new Range(new Position(0, 0), new Position(2, 14)));
+
+    assert.strictEqual(extractLastNameDefinitions.length, 1);
+    assert.ok(extractLastNameDefinitions[0].targetUri.path.endsWith('/test-fixture/macros/name_utils.sql'));
+    assert.deepStrictEqual(extractLastNameDefinitions[0].originSelectionRange, new Range(new Position(3, 7), new Position(3, 24)));
+    assert.deepStrictEqual(extractLastNameDefinitions[0].targetSelectionRange, new Range(new Position(4, 9), new Position(4, 26)));
+    assert.deepStrictEqual(extractLastNameDefinitions[0].targetRange, new Range(new Position(4, 0), new Position(6, 14)));
+  });
+});
 
 // suite('source definitions', () => {});
