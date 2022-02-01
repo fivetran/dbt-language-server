05c52b28bf10d760cef0489170bd621d514c6a59
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-02-01 15:04:55 +0300

    Simplify go to definition feature for sources

--- server/src/definition/SourceDefinitionFinder.ts
+++ server/src/definition/SourceDefinitionFinder.ts
@@ -1,4 +1,3 @@
-import * as fs from 'fs';
 import * as path from 'path';
 import { DefinitionLink, integer, LocationLink, Position, Range } from 'vscode-languageserver';
 import { TextDocument } from 'vscode-languageserver-textdocument';
@@ -50,7 +49,12 @@ export class SourceDefinitionFinder {
       );
 
       if (positionInRange(position, sourceSelectionRange)) {
-        return this.getSourceDefinitions(source.text.substring(1).slice(0, -1), dbtSources, sourceSelectionRange);
+        return this.getTableDefinitions(
+          source.text.substring(1).slice(0, -1),
+          table.text.substring(1).slice(0, -1),
+          dbtSources,
+          sourceSelectionRange,
+        );
       } else if (positionInRange(position, tableSelectionRange)) {
         return this.getTableDefinitions(source.text.substring(1).slice(0, -1), table.text.substring(1).slice(0, -1), dbtSources, tableSelectionRange);
       }
@@ -59,79 +63,19 @@ export class SourceDefinitionFinder {
     return undefined;
   }
 
-  getSourceDefinitions(source: string, dbtSources: ManifestSource[], sourceSelectionRange: Range): DefinitionLink[] | undefined {
-    const sources = dbtSources.filter(s => s.sourceName === source);
-    if (sources.length === 0) {
-      return undefined;
-    }
-
-    const sourceDefinitionFilePath = path.join(sources[0].rootPath, sources[0].originalFilePath);
-    const sourceDefinitionFileContent = fs.readFileSync(sourceDefinitionFilePath, 'utf8');
-
-    return sources.map(s => {
-      const [definitionRange, selectionRange] = this.getSourceRanges(s.sourceName, s.name, s, sourceDefinitionFileContent);
-      return LocationLink.create(sourceDefinitionFilePath, definitionRange, selectionRange, sourceSelectionRange);
-    });
-  }
-
   getTableDefinitions(source: string, table: string, dbtSources: ManifestSource[], tableSelectionRange: Range): DefinitionLink[] | undefined {
     const sourcesSearch = dbtSources.filter(s => s.sourceName === source && s.name === table);
     if (sourcesSearch.length === 0) {
       return undefined;
     }
-
     const [selectedSource] = sourcesSearch;
-    const sourceDefinitionFilePath = path.join(selectedSource.rootPath, selectedSource.originalFilePath);
-    const sourceDefinitionFileContent = fs.readFileSync(sourceDefinitionFilePath, 'utf8');
-    const [definitionRange, selectionRange] = this.getSourceRanges(
-      selectedSource.sourceName,
-      selectedSource.name,
-      selectedSource,
-      sourceDefinitionFileContent,
-    );
-
     return [
-      LocationLink.create(path.join(selectedSource.rootPath, selectedSource.originalFilePath), definitionRange, selectionRange, tableSelectionRange),
-    ];
-  }
-
-  // todo: rewrite candidate
-  // todo: span definition on all table
-  getSourceRanges(sourceName: string, tableName: string, dbtSource: ManifestSource, definitionFileContent: string): [Range, Range] {
-    const definitionFileContentLines = definitionFileContent.split('\n');
-    let levelIndentation: number | undefined = undefined;
-    let level: 'up' | 'sources' | 'tables' = 'up';
-    let sourceMatch = false;
-
-    for (let i = 0; i < definitionFileContentLines.length; i++) {
-      const line = definitionFileContentLines[i];
-      const lineContentMatch = /\S+/.exec(line);
-      if (lineContentMatch) {
-        const lineIndentation = lineContentMatch.index;
-        levelIndentation = levelIndentation ?? lineIndentation;
-
-        if (line.includes('sources:')) {
-          level = 'sources';
-          levelIndentation = undefined;
-        } else if (line.includes('tables:')) {
-          level = 'tables';
-          levelIndentation = undefined;
-        } else if (line.includes('name:')) {
-          if (level === 'sources') {
-            sourceMatch = line.includes(sourceName);
-          } else if (level === 'tables' && line.includes(tableName) && sourceMatch) {
-            return [
-              Range.create(Position.create(i, levelIndentation), Position.create(i, line.length)),
-              Range.create(Position.create(i, line.indexOf(tableName)), Position.create(i, line.indexOf(tableName) + tableName.length)),
-            ];
-          }
-        }
-      }
-    }
-
-    return [
-      Range.create(Position.create(0, 0), Position.create(integer.MAX_VALUE, integer.MAX_VALUE)),
-      Range.create(Position.create(0, 0), Position.create(0, 0)),
+      LocationLink.create(
+        path.join(selectedSource.rootPath, selectedSource.originalFilePath),
+        Range.create(Position.create(0, 0), Position.create(integer.MAX_VALUE, integer.MAX_VALUE)),
+        Range.create(Position.create(0, 0), Position.create(0, 0)),
+        tableSelectionRange,
+      ),
     ];
   }
 }
