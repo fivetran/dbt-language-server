6ab0b0899da1d8f4cdf330143879119b173c8f7e
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-02-07 00:56:10 +0300

    Refactor e2e tests

--- e2e/src/definition.spec.ts
+++ e2e/src/definition.spec.ts
@@ -1,6 +1,9 @@
-import assert = require('assert');
-import { Position, Range } from 'vscode';
+import { assertThat, endsWith, greaterThan, is } from 'hamjest';
+import { DefinitionLink, Position, Range, Uri } from 'vscode';
 import { activateAndWait, getDocUri, triggerDefinition } from './helper';
+
+const MIN_RANGE = new Range(0, 0, 0, 0);
+const MAX_RANGE = new Range(0, 0, 2147483647, 2147483647);
 
 const refSqlDocUri = getDocUri('ref_sql.sql');
 const packageRefDocUri = getDocUri('package_ref.sql');
@@ -14,9 +17,9 @@ suite('ref definitions', () => {
     const definitions = await triggerDefinition(refSqlDocUri, new Position(1, 24));
 
     // assert
-    assert.strictEqual(definitions.length, 1);
-    assert.ok(definitions[0].targetUri.path.endsWith('/test-fixture/models/table_exists.sql'));
-    assert.deepStrictEqual(definitions[0].originSelectionRange, new Range(new Position(1, 19), new Position(1, 31)));
+    assertThat(definitions.length, 1);
+    assertThat(definitions[0].targetUri.path, endsWith('/test-fixture/models/table_exists.sql'));
+    assertThat(definitions[0].originSelectionRange, new Range(1, 19, 1, 31));
   });
 
   test('Should suggest definitions for ref with package', async () => {
@@ -28,50 +31,60 @@ suite('ref definitions', () => {
     const modelDefinitions = await triggerDefinition(packageRefDocUri, new Position(5, 42));
 
     // assert
-    assert.ok(packageDefinitions.length > 1);
-    assert.deepStrictEqual(packageDefinitions[0].originSelectionRange, new Range(new Position(5, 19), new Position(5, 33)));
+    assertThat(packageDefinitions.length, is(greaterThan(1)));
+    assertThat(packageDefinitions[0].originSelectionRange, new Range(5, 19, 5, 33));
 
-    assert.strictEqual(modelDefinitions.length, 1);
-    assert.ok(modelDefinitions[0].targetUri.path.endsWith('/test-fixture/models/table_exists.sql'));
-    assert.deepStrictEqual(modelDefinitions[0].originSelectionRange, new Range(new Position(5, 37), new Position(5, 49)));
+    assertThat(modelDefinitions.length, 1);
+    assertThat(modelDefinitions[0].targetUri.path, endsWith('/test-fixture/models/table_exists.sql'));
+    assertThat(modelDefinitions[0].originSelectionRange, new Range(5, 37, 5, 49));
   });
 });
 
 suite('macro definitions', () => {
   test('Should suggest definitions for macros', async () => {
-    // arrange
     await activateAndWait(packageRefDocUri);
-
-    // act
-    const extractFirstNameDefinitions = await triggerDefinition(packageRefDocUri, new Position(2, 9));
-    const extractLastNameDefinitions = await triggerDefinition(packageRefDocUri, new Position(3, 9));
-
-    // assert
-    assert.strictEqual(extractFirstNameDefinitions.length, 1);
-    assert.ok(extractFirstNameDefinitions[0].targetUri.path.endsWith('/test-fixture/macros/name_utils.sql'));
-    assert.deepStrictEqual(extractFirstNameDefinitions[0].originSelectionRange, new Range(new Position(2, 7), new Position(2, 25)));
-    assert.deepStrictEqual(extractFirstNameDefinitions[0].targetSelectionRange, new Range(new Position(0, 9), new Position(0, 27)));
-    assert.deepStrictEqual(extractFirstNameDefinitions[0].targetRange, new Range(new Position(0, 0), new Position(2, 14)));
-
-    assert.strictEqual(extractLastNameDefinitions.length, 1);
-    assert.ok(extractLastNameDefinitions[0].targetUri.path.endsWith('/test-fixture/macros/name_utils.sql'));
-    assert.deepStrictEqual(extractLastNameDefinitions[0].originSelectionRange, new Range(new Position(3, 7), new Position(3, 24)));
-    assert.deepStrictEqual(extractLastNameDefinitions[0].targetSelectionRange, new Range(new Position(4, 9), new Position(4, 26)));
-    assert.deepStrictEqual(extractLastNameDefinitions[0].targetRange, new Range(new Position(4, 0), new Position(6, 14)));
+    await assertDefinitions(packageRefDocUri, new Position(2, 9), [
+      {
+        originSelectionRange: new Range(2, 7, 2, 25),
+        targetUri: getDocUri('/macros/name_utils.sql'),
+        targetRange: new Range(0, 0, 2, 14),
+        targetSelectionRange: new Range(0, 9, 0, 27),
+      },
+    ]);
+    await assertDefinitions(packageRefDocUri, new Position(3, 9), [
+      {
+        originSelectionRange: new Range(3, 7, 3, 24),
+        targetUri: getDocUri('/macros/name_utils.sql'),
+        targetRange: new Range(4, 0, 6, 14),
+        targetSelectionRange: new Range(4, 9, 4, 26),
+      },
+    ]);
   });
 });
 
 suite('source definitions', () => {
   test('Should suggest definitions for source', async () => {
-    // arrange
     await activateAndWait(packageRefDocUri);
-
-    // act
-    const sourceDefinitions = await triggerDefinition(packageRefDocUri, new Position(4, 33));
-
-    // assert
-    assert.strictEqual(sourceDefinitions.length, 1);
-    assert.ok(sourceDefinitions[0].targetUri.path.endsWith('/test-fixture/models/sources/new_project.yml'));
-    assert.deepStrictEqual(sourceDefinitions[0].originSelectionRange, new Range(new Position(4, 31), new Position(4, 36)));
+    await assertDefinitions(packageRefDocUri, new Position(4, 33), [
+      {
+        originSelectionRange: new Range(4, 31, 4, 36),
+        targetUri: getDocUri('/sources/new_project.yml'),
+        targetRange: MIN_RANGE,
+        targetSelectionRange: MAX_RANGE,
+      },
+    ]);
   });
 });
+
+async function assertDefinitions(docUri: Uri, position: Position, expectedDefinitions: DefinitionLink[]): Promise<void> {
+  const definitions = await triggerDefinition(docUri, position);
+
+  assertThat(definitions.length, expectedDefinitions.length);
+
+  for (let i = 0; i < 0; i++) {
+    assertThat(definitions[i].originSelectionRange, expectedDefinitions[i].originSelectionRange);
+    assertThat(definitions[i].targetUri, expectedDefinitions[i].targetUri);
+    assertThat(definitions[i].targetRange, expectedDefinitions[i].targetRange);
+    assertThat(definitions[i].targetSelectionRange, expectedDefinitions[i].targetSelectionRange);
+  }
+}
