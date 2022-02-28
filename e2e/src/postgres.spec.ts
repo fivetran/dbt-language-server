3a7ddc880b1538de569c0b7b7c1932d794d50b5e
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-02-28 17:19:10 +0300

    Revert back changes related to postgres tests

--- e2e/src/postgres.spec.ts
+++ e2e/src/postgres.spec.ts
@@ -1,10 +1,10 @@
-import { assertThat } from 'hamjest';
-import { activateAndWait, getCustomDocUri, getPreviewText } from './helper';
+// import { assertThat } from 'hamjest';
+// import { activateAndWait, getCustomDocUri, getPreviewText } from './helper';
 
-suite('Postgres destination', () => {
-  test('Should work with postgres destination', async () => {
-    await activateAndWait(getCustomDocUri('postgres/models/version.sql'));
+// suite('Postgres destination', () => {
+//   test('Should work with postgres destination', async () => {
+//     await activateAndWait(getCustomDocUri('postgres/models/version.sql'));
 
-    assertThat(getPreviewText(), '0.20.1');
-  });
-});
+//     assertThat(getPreviewText(), '0.20.1');
+//   });
+// });
