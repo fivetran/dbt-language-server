1dcdbf1ad9c54dc74ee1487335d8c3096ed5f7a2
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-03-18 14:30:13 +0300

    Show destination initialization error

--- server/src/LspServer.ts
+++ server/src/LspServer.ts
@@ -141,6 +141,7 @@ export class LspServer {
 
     const initializeDestinationResult = await this.initializeDestination();
     if (initializeDestinationResult.isErr()) {
+      this.connection.window.showWarningMessage(initializeDestinationResult.error.message);
       this.connection.window.showWarningMessage(
         `Only common dbt features will be available. Dbt profile was not configured. ${initializeDestinationResult.error}`,
       );
