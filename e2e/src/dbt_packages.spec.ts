ffef60fdd1d3e5652b0979d66b833598c00094e5
Pavel Grivachev <pavel@fivetran.com>
2022-03-28 19:40:24 +0300

    fix for old dbt version

--- e2e/src/dbt_packages.spec.ts
+++ e2e/src/dbt_packages.spec.ts
@@ -1,13 +1,23 @@
 import { assertThat, containsString } from 'hamjest';
+import { workspace } from 'vscode';
 import { activateAndWait, getCustomDocUri, getPreviewText, installDbtPackages } from './helper';
 
 suite('Extension should work inside dbt package', () => {
   const PROJECT = 'project-with-packages';
 
+  const FILE_PATH_NEW = `${PROJECT}/dbt_packages/salesforce_source/models/stg_salesforce__user.sql`;
+  const FILE_PATH_OLD = `${PROJECT}/dbt_modules/salesforce_source/models/stg_salesforce__user.sql`;
+
   test('Should compile model inside package', async () => {
     // arrange, act
     installDbtPackages(PROJECT);
-    const docUri = getCustomDocUri(`${PROJECT}/dbt_packages/salesforce_source/models/stg_salesforce__user.sql`);
+    let docUri;
+    try {
+      docUri = getCustomDocUri(FILE_PATH_NEW);
+      await workspace.fs.stat(docUri);
+    } catch (e) {
+      docUri = getCustomDocUri(FILE_PATH_OLD); // TODO: delete this after getting rid of old dbt version
+    }
     await activateAndWait(docUri);
 
     // assert
