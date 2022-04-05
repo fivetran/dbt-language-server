6427cc8363f4749a8b466b4c2d1d07021ff0d176
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-04-05 23:33:59 +0300

    Provide sources names as autocompletions

--- server/src/completion/SourceCompletionProvider.ts
+++ server/src/completion/SourceCompletionProvider.ts
@@ -6,26 +6,34 @@ export class SourceCompletionProvider implements DbtNodeCompletionProvider {
   static readonly SOURCE_PATTERN = /source\s*\(\s*['|"]?$/;
   static readonly TABLE_PATTERN = /source\s*\(\s*('[^)']*'|"[^)"]*")\s*,\s*('|")$/;
 
-  static readonly CURRENT_PACKAGE_SORT_PREFIX = '1';
-  static readonly INSTALLED_PACKAGE_SORT_PREFIX = '2';
-
   constructor(private dbtRepository: DbtRepository) {}
 
   provideCompletions(jinjaBeforePositionText: string): Promise<CompletionItem[] | undefined> {
     const sourceMatch = SourceCompletionProvider.SOURCE_PATTERN.exec(jinjaBeforePositionText);
     if (sourceMatch) {
       const lastChar = jinjaBeforePositionText.charAt(jinjaBeforePositionText.length - 1);
-      return Promise.resolve(
-        this.dbtRepository.sources.map<CompletionItem>(s => {
-          const label = `(${s.packageName}) ${s.sourceName}.${s.name}`;
-          const insertText = this.getSourceInsertText(s.sourceName, s.name, lastChar);
-          const sortOrder =
-            this.dbtRepository.projectName === s.packageName
-              ? `${SourceCompletionProvider.CURRENT_PACKAGE_SORT_PREFIX}_${label}`
-              : `${SourceCompletionProvider.INSTALLED_PACKAGE_SORT_PREFIX}_${label}`;
-          return this.getSourceCompletionItem(label, insertText, 'Source', sortOrder);
-        }),
-      );
+
+      // todo: move to files change listener
+      const uniqueSources = new Map<string, Set<string>>();
+      this.dbtRepository.sources.forEach(s => {
+        let sourceNames = uniqueSources.get(s.packageName);
+        if (!sourceNames) {
+          sourceNames = new Set<string>();
+          uniqueSources.set(s.packageName, sourceNames);
+        }
+        sourceNames.add(s.name);
+      });
+
+      const completionItems = [];
+      for (const completionEntry of uniqueSources.entries()) {
+        for (const name of completionEntry[1]) {
+          const label = `(${completionEntry[0]}) ${name}`;
+          const insertText = this.getSourceInsertText(name, lastChar);
+          completionItems.push(this.getSourceCompletionItem(label, insertText, 'Source'));
+        }
+      }
+
+      return Promise.resolve(completionItems);
     }
 
     const tableMatch = SourceCompletionProvider.TABLE_PATTERN.exec(jinjaBeforePositionText);
@@ -36,12 +44,7 @@ export class SourceCompletionProvider implements DbtNodeCompletionProvider {
           .filter(s => s.sourceName === sourceName)
           .map<CompletionItem>(s => {
             const label = this.dbtRepository.projectName === s.packageName ? s.name : `(${s.packageName}) ${s.name}`;
-            const insertText = s.name;
-            const sortOrder =
-              this.dbtRepository.projectName === s.packageName
-                ? `${SourceCompletionProvider.CURRENT_PACKAGE_SORT_PREFIX}_${label}`
-                : `${SourceCompletionProvider.INSTALLED_PACKAGE_SORT_PREFIX}_${label}`;
-            return this.getSourceCompletionItem(label, insertText, 'Table', sortOrder);
+            return this.getSourceCompletionItem(label, s.name, 'Table');
           }),
       );
     }
@@ -49,17 +52,16 @@ export class SourceCompletionProvider implements DbtNodeCompletionProvider {
     return Promise.resolve(undefined);
   }
 
-  private getSourceCompletionItem(label: string, insertText: string, detail: 'Source' | 'Table', sortText?: string): CompletionItem {
+  private getSourceCompletionItem(label: string, insertText: string, detail: 'Source' | 'Table'): CompletionItem {
     return {
       label,
       insertText,
-      sortText,
       kind: CompletionItemKind.Value,
       detail,
     };
   }
 
-  private getSourceInsertText(sourceName: string, tableName: string, lastChar: string): string {
-    return lastChar === "'" ? `${sourceName}', '${tableName}` : `'${sourceName}', '${tableName}'`;
+  private getSourceInsertText(sourceName: string, lastChar: string): string {
+    return lastChar === "'" ? sourceName : `'${sourceName}'`;
   }
 }
