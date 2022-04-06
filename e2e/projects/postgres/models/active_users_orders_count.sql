700fb47706b518b03738f776fe92cd13b4b11cfb
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-04-06 16:46:40 +0300

    Add e2e tests for macros autocompletions

--- e2e/projects/postgres/models/active_users_orders_count.sql
+++ e2e/projects/postgres/models/active_users_orders_count.sql
@@ -1,4 +1,4 @@
-select u.id, {{ extract_first_name('u.full_name') }} as first_name, {{ extract_last_name('u.full_name') }} as last_name, count(*)
+select u.id, {{ extract_first_name('u.full_name') }} as first_name, {{ dbt_postgres_test.extract_last_name('u.full_name') }} as last_name, count(*)
 from {{ ref('dbt_postgres_test', 'active_users') }} u
     join orders o on u.id = o.user_id
 group by u.id, u.full_name
---
+++ e2e/src/completion/completion_macros.spec.ts
@@ -0,0 +1,42 @@
+import { assertThat, defined } from 'hamjest';
+import { CompletionItem, CompletionItemKind, Position } from 'vscode';
+import { activateAndWait, getCustomDocUri, testCompletion, triggerCompletion } from '../helper';
+
+suite('Should suggest macros completions', () => {
+  const PROJECT_FILE_NAME = 'postgres/models/active_users_orders_count.sql';
+
+  test('Should suggest macros', async () => {
+    // arrange
+    const docUri = getCustomDocUri(PROJECT_FILE_NAME);
+    await activateAndWait(docUri);
+
+    // act
+    const actualCompletionList = await triggerCompletion(docUri, new Position(0, 15), 'e');
+
+    // assert
+    const expectedCompletions = getMacrosCompletionList();
+    expectedCompletions.items.forEach(c => {
+      const actualCompletion = actualCompletionList.items.find(a => a.label === c.label && a.insertText === c.insertText);
+      assertThat(actualCompletion, defined());
+    });
+  });
+
+  test('Should suggest macros from package', async () => {
+    const docUri = getCustomDocUri(PROJECT_FILE_NAME);
+    await activateAndWait(docUri);
+    await testCompletion(docUri, new Position(0, 89), getMacrosCompletionList());
+  });
+
+  function getMacrosCompletionList(): { items: CompletionItem[] } {
+    return {
+      items: getMacrosCompletions().map<CompletionItem>(c => ({ label: c[0], insertText: c[1], kind: CompletionItemKind.Value })),
+    };
+  }
+
+  function getMacrosCompletions(): [string, string][] {
+    return [
+      ['extract_first_name', 'extract_first_name'],
+      ['extract_last_name', 'extract_last_name'],
+    ];
+  }
+});
