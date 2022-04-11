57410463ac7df7a28b9cd37f7d0c8b2f56c08963
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-04-11 15:16:04 +0300

    Refactoring

--- e2e/src/completion/completion_sources.spec.ts
+++ e2e/src/completion/completion_sources.spec.ts
@@ -5,30 +5,29 @@ import { activateAndWait, getCustomDocUri, getTextInQuotesIfNeeded } from '../he
 suite('Should suggest sources completions', () => {
   const PROJECT_FILE_NAME = 'postgres/models/active_users.sql';
 
+  const SOURCE_COMPLETIONS: [string, string][] = [['(dbt_postgres_test) users_orders', 'users_orders']];
+  const TABLE_COMPLETIONS: [string, string][] = [
+    ['orders', 'orders'],
+    ['users', 'users'],
+  ];
+
   test('Should suggest sources', async () => {
     const docUri = getCustomDocUri(PROJECT_FILE_NAME);
     await activateAndWait(docUri);
-    await assertCompletions(docUri, new Position(1, 16), getSourcesCompletionList(getSourcesCompletions, false));
+    await assertCompletions(docUri, new Position(1, 16), getSourcesCompletionList(SOURCE_COMPLETIONS, false));
   });
 
   test('Should suggest source tables', async () => {
     const docUri = getCustomDocUri(PROJECT_FILE_NAME);
     await activateAndWait(docUri);
-    await assertCompletions(docUri, new Position(1, 32), getSourcesCompletionList(getTablesCompletions, false));
+    await assertCompletions(docUri, new Position(1, 32), getSourcesCompletionList(TABLE_COMPLETIONS, false));
   });
 
-  function getSourcesCompletionList(getCompletions: (withQuotes: boolean) => [string, string][], withQuotes: boolean): CompletionItem[] {
-    return getCompletions(withQuotes).map<CompletionItem>(c => ({ label: c[0], insertText: c[1], kind: CompletionItemKind.Value }));
-  }
-
-  function getSourcesCompletions(withQuotes: boolean): [string, string][] {
-    return [['(dbt_postgres_test) users_orders', getTextInQuotesIfNeeded('users_orders', withQuotes)]];
-  }
-
-  function getTablesCompletions(withQuotes: boolean): [string, string][] {
-    return [
-      ['orders', getTextInQuotesIfNeeded('orders', withQuotes)],
-      ['users', getTextInQuotesIfNeeded('users', withQuotes)],
-    ];
+  function getSourcesCompletionList(completions: [string, string][], withQuotes: boolean): CompletionItem[] {
+    return completions.map<CompletionItem>(c => ({
+      label: c[0],
+      insertText: getTextInQuotesIfNeeded(c[1], withQuotes),
+      kind: CompletionItemKind.Value,
+    }));
   }
 });
