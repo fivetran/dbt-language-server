fcd622999bb4bae7eb7c81873abdb0542b482bad
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-04-14 14:49:54 +0300

    Add e2e test to check recursive queries support

--- e2e/projects/test-fixture/models/referrers.sql
+++ e2e/projects/test-fixture/models/referrers.sql
@@ -7,7 +7,7 @@ with recursive referrers as (
     select rf1.id as user_id, rf2.referrer_id as referrer_id
     from referrers rf1
         inner join referrers rf2 on rf1.referrer_id = rf2.id
-    where not exists (select * from referrers where user_id = rf1.id as user_id and referrer_id = rf2.referrer_id)
+    where not exists (select * from referrers where user_id = rf1.id and referrer_id = rf2.referrer_id)
 )
 
 select *
