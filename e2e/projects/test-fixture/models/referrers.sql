1cd6cc955d8910d30dc92a30cf6b8c23d275946d
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-04-14 14:33:10 +0300

    Add new model to test recursive cte support

---
+++ e2e/projects/test-fixture/models/referrers.sql
@@ -0,0 +1,14 @@
+with recursive referrers as (
+    select id as user_id, referrer_id
+    from {{ source('new_project', 'users') }}
+
+    union all
+
+    select rf1.id as user_id, rf2.referrer_id as referrer_id
+    from referrers rf1
+        inner join referrers rf2 on rf1.referrer_id = rf2.id
+    where not exists (select * from referrers where user_id = rf1.id as user_id and referrer_id = rf2.referrer_id)
+)
+
+select *
+from referrers
