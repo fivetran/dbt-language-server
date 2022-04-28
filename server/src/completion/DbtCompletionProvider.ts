a0f0ec898e86d61f1b9d3c76ba7514140d2858e8
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-04-28 18:36:00 +0300

    Log dbt completions

--- server/src/completion/DbtCompletionProvider.ts
+++ server/src/completion/DbtCompletionProvider.ts
@@ -21,10 +21,30 @@ export class DbtCompletionProvider {
   }
 
   async provideCompletions(jinjaPartType: JinjaPartType, jinjaBeforePositionText: string): Promise<CompletionItem[] | undefined> {
-    return (
-      (await this.modelCompletionProvider.provideCompletions(jinjaPartType, jinjaBeforePositionText)) ??
-      (await this.sourceCompletionProvider.provideCompletions(jinjaPartType, jinjaBeforePositionText)) ??
-      (await this.macroCompletionProvider.provideCompletions(jinjaPartType, jinjaBeforePositionText))
-    );
+    console.log('--- provideCompletions start ---');
+    console.log(`jinjaPartType: ${JinjaPartType[jinjaPartType]}`);
+    console.log(`jinjaBeforePositionText: ${jinjaBeforePositionText}`);
+    console.log('--- provideCompletions end ---');
+
+    const modelCompletions = await this.modelCompletionProvider.provideCompletions(jinjaPartType, jinjaBeforePositionText);
+    if (modelCompletions) {
+      console.log(`modelCompletions: ${modelCompletions}`);
+      return modelCompletions;
+    }
+
+    const sourceCompletions = await this.sourceCompletionProvider.provideCompletions(jinjaPartType, jinjaBeforePositionText);
+    if (sourceCompletions) {
+      console.log(`sourceCompletions: ${sourceCompletions}`);
+      return sourceCompletions;
+    }
+
+    const macroCompletions = await this.macroCompletionProvider.provideCompletions(jinjaPartType, jinjaBeforePositionText);
+    if (macroCompletions) {
+      console.log(`macroCompletions: ${macroCompletions}`);
+      return macroCompletions;
+    }
+
+    console.log('dbt completions not found');
+    return undefined;
   }
 }
