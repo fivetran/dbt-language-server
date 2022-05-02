75659356bcda57b1833c9965ee1ae435da46058c
Pavel Grivachev <pavel@fivetran.com>
2022-05-02 14:02:41 +0300

    fix unsafe access

--- client/src/PythonExtension.ts
+++ client/src/PythonExtension.ts
@@ -1,4 +1,14 @@
-import { extensions, WorkspaceFolder } from 'vscode';
+import { extensions, Uri, WorkspaceFolder } from 'vscode';
+
+interface IExtensionApi {
+  settings: {
+    getExecutionDetails(resource?: Resource): {
+      execCommand: string[] | undefined;
+    };
+  };
+}
+
+type Resource = Uri | undefined;
 
 export class PythonExtension {
   async getPython(workspaceFolder?: WorkspaceFolder): Promise<string> {
@@ -12,7 +22,13 @@ export class PythonExtension {
       await extension.activate();
     }
 
-    const [path] = extension.exports.settings.getExecutionDetails(workspaceFolder?.uri).execCommand as [string];
+    const details = (extension.exports as IExtensionApi).settings.getExecutionDetails(workspaceFolder?.uri);
+    if (!details.execCommand) {
+      console.log('ms-python.python not found');
+      return '';
+    }
+
+    const [path] = details.execCommand;
 
     console.log(`Python path used: ${path}`);
     return path;
