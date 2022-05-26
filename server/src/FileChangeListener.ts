b3364ede20a198e6a4bbced86070925e040447bb
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-05-27 00:45:31 +0200

    Revert "Prevent dbt_project.yml file parsing multiple times"
    This reverts commit d373ded5bac491a32cd9b11ab72ac5fbdfead7f5.

--- server/src/FileChangeListener.ts
+++ server/src/FileChangeListener.ts
@@ -38,12 +38,11 @@ export class FileChangeListener {
   }
 
   updateDbtProjectConfig(): void {
-    const dbtProject = this.dbtProject.getProject();
-    this.dbtRepository.dbtTargetPath = this.dbtProject.findTargetPath(dbtProject);
-    this.dbtRepository.projectName = this.dbtProject.findProjectName(dbtProject);
-    this.dbtRepository.macroPaths = this.dbtProject.findMacroPaths(dbtProject);
-    this.dbtRepository.modelPaths = this.dbtProject.findModelPaths(dbtProject);
-    this.dbtRepository.packagesInstallPaths = this.dbtProject.findPackagesInstallPaths(dbtProject);
+    this.dbtRepository.dbtTargetPath = this.dbtProject.findTargetPath();
+    this.dbtRepository.projectName = this.dbtProject.findProjectName();
+    this.dbtRepository.macroPaths = this.dbtProject.findMacroPaths();
+    this.dbtRepository.modelPaths = this.dbtProject.findModelPaths();
+    this.dbtRepository.packagesInstallPaths = this.dbtProject.findPackagesInstallPaths();
   }
 
   updateManifestNodes(): void {
