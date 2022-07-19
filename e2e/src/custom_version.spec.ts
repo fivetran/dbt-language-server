26d43e747349c78915f7ef93253573096b38ac48
Pavel Grivachev <pavel@fivetran.com>
2022-07-19 11:49:08 +0200

    fix suite name

--- e2e/src/custom_version.spec.ts
+++ e2e/src/custom_version.spec.ts
@@ -1,7 +1,7 @@
 import { assertThat } from 'hamjest';
 import { activateAndWait, getCustomDocUri, getPreviewText } from './helper';
 
-suite('Multi-project', () => {
+suite('Custom version', () => {
   test('Should run project with dbt version specified for workspace', async () => {
     await activateAndWait(getCustomDocUri('special-python-settings/models/version.sql'));
 
