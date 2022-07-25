0084a4b55db5c2eb748456a16e4c91de2f3e50ca
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-07-25 22:05:46 +0200

    Revert "Temporary skip test"
    This reverts commit 66b9f62ca2cf30771f8d79d01c8953696a99513a.

--- e2e/src/runners/index.ts
+++ e2e/src/runners/index.ts
@@ -1,6 +1,6 @@
 import { indexMain } from './indexMain';
 
 export async function run(): Promise<void> {
-  const testsToSkip = (process.env['SKIP_TESTS']?.split(',') ?? []).concat('dbt_ft.spec.js').concat('dbt_error.spec.ts');
+  const testsToSkip = (process.env['SKIP_TESTS']?.split(',') ?? []).concat('dbt_ft.spec.js');
   return indexMain('70s', '../**/*.spec.js', testsToSkip);
 }
