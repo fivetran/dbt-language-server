1e291f40e8e180f4b3c67ee7b0ed19f544675c73
Pavel Grivachev <pavel@fivetran.com>
2022-07-25 17:13:39 +0200

    use stderr and common patern

--- server/src/test/FeatureFinder.spec.ts
+++ server/src/test/FeatureFinder.spec.ts
@@ -17,8 +17,25 @@ describe('FeatureFinder', () => {
                             - databricks: 1.1.1 - Up to date!
                             - bigquery:   1.1.1 - Up to date!
                             - spark:      1.1.0 - Up to date!`;
+
+  const LEGACY_VERSION_OUTPU = `installed version: 0.20.1
+                                  latest version: 1.0.0
+
+                                Your version of dbt is out of date! You can find instructions for upgrading here:
+                                https://docs.getdbt.com/docs/installation
+
+                                Plugins:
+                                - bigquery: 0.20.1
+                                - snowflake: 0.20.1
+                                - redshift: 0.20.1
+                                - postgres: 0.20.1`;
   const VERSION_COMMAND_RESULT = Promise.resolve({
     stderr: VERSION_OUTPUT,
+    stdout: '',
+  });
+
+  const LEGACY_VERSION_COMMAND_RESULT = Promise.resolve({
+    stderr: LEGACY_VERSION_OUTPU,
     stdout: '',
   });
 
@@ -44,6 +61,24 @@ describe('FeatureFinder', () => {
       { name: 'databricks', version: { major: 1, minor: 1, patch: 1 } },
       { name: 'bigquery', version: { major: 1, minor: 1, patch: 1 } },
       { name: 'spark', version: { major: 1, minor: 1, patch: 0 } },
+    ]);
+    assertThat(result[1], not(defined()));
+    assertThat(result[2], not(defined()));
+  });
+
+  it('getAvailableDbt should return all information about installed dbt and adapters for legacy dbt version', async () => {
+    // arrange
+    when(mockCommandExecutor.execute(deepEqual(RPC_WITH_PYTHON))).thenReturn(LEGACY_VERSION_COMMAND_RESULT);
+
+    // act
+    const result = await new FeatureFinder({ path: PYTHON_PATH }, instance(mockCommandExecutor)).getAvailableDbt();
+
+    // assert
+    assertVersion(result[0], { major: 0, minor: 20, patch: 1 }, { major: 1, minor: 0, patch: 0 }, [
+      { name: 'bigquery', version: { major: 0, minor: 20, patch: 1 } },
+      { name: 'snowflake', version: { major: 0, minor: 20, patch: 1 } },
+      { name: 'redshift', version: { major: 0, minor: 20, patch: 1 } },
+      { name: 'postgres', version: { major: 0, minor: 20, patch: 1 } },
     ]);
     assertThat(result[1], not(defined()));
     assertThat(result[2], not(defined()));
