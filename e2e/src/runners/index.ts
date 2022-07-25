66b9f62ca2cf30771f8d79d01c8953696a99513a
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-07-25 21:48:48 +0200

    Temporary skip test

--- e2e/src/runners/index.ts
+++ e2e/src/runners/index.ts
@@ -1,6 +1,6 @@
 import { indexMain } from './indexMain';
 
 export async function run(): Promise<void> {
-  const testsToSkip = (process.env['SKIP_TESTS']?.split(',') ?? []).concat('dbt_ft.spec.js');
+  const testsToSkip = (process.env['SKIP_TESTS']?.split(',') ?? []).concat('dbt_ft.spec.js').concat('dbt_error.spec.ts');
   return indexMain('70s', '../**/*.spec.js', testsToSkip);
 }
