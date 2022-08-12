fd3ed0f926a27e38c80caa9af87b06ba9a8a4dd9
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-08-12 13:28:37 +0200

    Log completions list

--- e2e/src/asserts.ts
+++ e2e/src/asserts.ts
@@ -65,6 +65,7 @@ export async function assertCompletions(
   triggerChar?: string,
 ): Promise<void> {
   const actualCompletionList = await triggerCompletion(docUri, position, triggerChar);
+  console.log(`Actual completion list: ${actualCompletionList.items.map<string>(c => c.insertText?.toString() ?? '').join(',')}`);
 
   assertThat(actualCompletionList.items.length, greaterThanOrEqualTo(expectedCompletionList.length));
   expectedCompletionList.forEach((expectedItem, i) => {
