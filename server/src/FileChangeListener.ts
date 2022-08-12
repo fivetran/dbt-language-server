b9ab087dc3fd3db2d14f99e52518062dfd4425f6
Pavel Grivachev <pavel@fivetran.com>
2022-08-12 20:03:02 +0200

    decrease number of manifest parse

--- server/src/FileChangeListener.ts
+++ server/src/FileChangeListener.ts
@@ -1,10 +1,10 @@
-import path = require('path');
-import { DidChangeWatchedFilesParams, Emitter, Event, FileChangeType } from 'vscode-languageserver';
+import { DidChangeWatchedFilesParams, Emitter, Event, FileChangeType, FileEvent } from 'vscode-languageserver';
 import { URI } from 'vscode-uri';
 import { DbtProject } from './DbtProject';
 import { DbtRepository } from './DbtRepository';
 import { ManifestParser } from './manifest/ManifestParser';
 import { debounce } from './utils/Utils';
+import path = require('path');
 
 export class FileChangeListener {
   private static PACKAGES_UPDATE_DEBOUNCE_TIMEOUT = 1000;
@@ -47,6 +47,11 @@ export class FileChangeListener {
     const manifestJsonPath = path.resolve(this.workspaceFolder, this.dbtRepository.dbtTargetPath, DbtRepository.DBT_MANIFEST_FILE_NAME);
     const packagesPaths = this.dbtRepository.packagesInstallPaths.map(p => path.resolve(this.workspaceFolder, p));
 
+    // For some paths we want to do action only once even we got several changes here
+    if (this.containsChangeWithPath(params.changes, manifestJsonPath)) {
+      this.updateManifestNodes();
+    }
+
     for (const change of params.changes) {
       const changePath = URI.parse(change.uri).fsPath;
       if (changePath === this.dbtProjectYmlPath) {
@@ -54,14 +59,21 @@ export class FileChangeListener {
         this.onDbtProjectYmlChangedEmitter.fire();
         this.updateDbtProjectConfig();
         this.updateManifestNodes();
-      } else if (changePath === manifestJsonPath) {
-        this.updateManifestNodes();
       } else if (changePath === this.packagesYmlPath) {
         this.onDbtPackagesYmlChangedEmitter.fire(change.type);
       } else if (packagesPaths.some(p => changePath === p)) {
         this.debouncedDbtPackagesChangedEmitter();
       }
     }
+  }
+
+  containsChangeWithPath(changes: FileEvent[], fsPath: string): boolean {
+    return (
+      changes.find(c => {
+        const changePath = URI.parse(c.uri).fsPath;
+        return changePath === fsPath;
+      }) !== undefined
+    );
   }
 
   updateDbtProjectConfig(): void {
