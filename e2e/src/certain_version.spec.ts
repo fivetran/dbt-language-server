da7e0eee93e92d1c2b4917fce4a1694fb18a6f93
Dmitry Boykov <93979055+fivetran-dmitryboykov@users.noreply.github.com>
2022-08-17 18:21:24 +0200

    Change e2e tests parameters (#326)
    * Change e2e tests parameters
    
    * Rename e2e test suite
    
    * Rename e2e test
    
    * Rename e2e test

--- e2e/src/certain_version.spec.ts
+++ e2e/src/certain_version.spec.ts
@@ -1,14 +1,14 @@
 import { assertThat } from 'hamjest';
 import { activateAndWait, executeInstallLatestDbt, getCustomDocUri, getLatestDbtVersion, getPreviewText, waitPreviewModification } from './helper';
 
-suite('Custom version', () => {
+suite('Certain version', () => {
   const DOC_URI = getCustomDocUri('special-python-settings/models/version.sql');
 
   test('Should run project with dbt version specified for workspace', async () => {
     await activateAndWait(DOC_URI);
 
     assertThat(getPreviewText(), '1.0.8');
-  });
+  }).timeout('100s');
 
   test('Should install latest dbt, restart language server and compile model with new dbt version', async () => {
     const latestVersion = getLatestDbtVersion();
