c13cd21f41e58a0042fd91c4450d55f8de6e2d68
Pavel Grivachev <pavel@fivetran.com>
2022-09-02 00:18:53 +0200

    don't open invisible documents

--- client/src/DbtLanguageClient.ts
+++ client/src/DbtLanguageClient.ts
@@ -1,6 +1,6 @@
 import { CustomInitParams, DbtCompilerType, LS_MANIFEST_PARSED_EVENT, StatusNotification, TelemetryEvent } from 'dbt-language-server-common';
 import { EventEmitter } from 'node:events';
-import { commands, Diagnostic, DiagnosticCollection, Disposable, RelativePattern, Uri, workspace } from 'vscode';
+import { commands, Diagnostic, DiagnosticCollection, Disposable, RelativePattern, TextDocument, Uri, window, workspace } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, State, TransportKind, WorkDoneProgress } from 'vscode-languageclient/node';
 import { ActiveTextEditorHandler } from './ActiveTextEditorHandler';
 import { DBT_PROJECT_YML, PACKAGES_YML, SUPPORTED_LANG_IDS } from './Constants';
@@ -60,6 +60,12 @@ export class DbtLanguageClient implements Disposable {
       synchronize: { fileEvents },
       outputChannel: outputChannelProvider.getMainLogChannel(),
       workspaceFolder: { uri: dbtProjectUri, name: dbtProjectUri.path, index: port },
+      middleware: {
+        didOpen: async (data: TextDocument, next: (data: TextDocument) => Promise<void>): Promise<void> =>
+          window.tabGroups.activeTabGroup.activeTab && data.uri.fsPath.endsWith(window.tabGroups.activeTabGroup.activeTab.label)
+            ? next(data)
+            : undefined,
+      },
     };
     disposables.push(...fileEvents);
     return clientOptions;
