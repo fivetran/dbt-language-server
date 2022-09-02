875976ff620abc2785d0ea5545d44dd62e1e0005
Pavel Grivachev <pavel@fivetran.com>
2022-09-02 22:09:06 +0200

    fix tests

--- e2e/src/runners/index.ts
+++ e2e/src/runners/index.ts
@@ -1,6 +1,10 @@
 import { indexMain } from './indexMain';
 
 export async function run(): Promise<void> {
-  const testsToSkip = [...(process.env['SKIP_TESTS']?.split(',') ?? []), 'dbt_ft.spec.js'];
+  const testsToSkip = ['dbt_ft.spec.js'];
+  const skipFromEnvString = process.env['SKIP_TESTS'];
+  if (skipFromEnvString) {
+    testsToSkip.push(...skipFromEnvString.split(','));
+  }
   return indexMain('70s', '../**/*.spec.js', testsToSkip);
 }
