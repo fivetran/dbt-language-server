a3b6fdccc6d0353d111398314ef11f4daf116fc1
Pavel Grivachev <pavel@fivetran.com>
2022-10-07 08:18:40 +0200

    improve test

--- e2e/src/editing_outside_jinja.spec.ts
+++ e2e/src/editing_outside_jinja.spec.ts
@@ -1,10 +1,18 @@
 import { assertThat } from 'hamjest';
-import { languages } from 'vscode';
+import { EOL } from 'node:os';
+import { DiagnosticSeverity, Range } from 'vscode';
 import { assertAllDiagnostics } from './asserts';
 import { activateAndWait, getCustomDocUri, getMainEditorText, getPreviewText, replaceText, setTestContent } from './helper';
 
 suite('Editing outside jinja without recompilation', () => {
   const DOC_URI = getCustomDocUri('completion-jinja/models/join_ref.sql');
+  const DIAGNOSTICS = [
+    {
+      message: `Compilation Error in model join_ref (models/join_ref.sql)${EOL}  unexpected '}', expected ')'${EOL}    line 6${EOL}      }}`,
+      range: new Range(5, 0, 5, 100),
+      severity: DiagnosticSeverity.Error,
+    },
+  ];
 
   test('Should not compile outside jinja if error exists', async () => {
     await activateAndWait(DOC_URI);
@@ -12,11 +20,11 @@ suite('Editing outside jinja without recompilation', () => {
     const initialPreview = getPreviewText();
 
     await replaceText('  )', '   ');
-    const dbtDiagnostics = languages.getDiagnostics(DOC_URI);
+    await assertAllDiagnostics(DOC_URI, DIAGNOSTICS);
 
     await replaceText('select u.id', 'select u.i');
     assertThat(getPreviewText(), getMainEditorText());
-    await assertAllDiagnostics(DOC_URI, dbtDiagnostics);
+    await assertAllDiagnostics(DOC_URI, DIAGNOSTICS);
 
     await setTestContent(initialContent);
     assertThat(getPreviewText(), initialPreview);
