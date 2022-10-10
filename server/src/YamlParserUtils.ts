a35b3ea4361ee39b4aabf4cfede9922334b8f061
Pavel Grivachev <pavel@fivetran.com>
2022-10-10 15:06:46 +0200

    replace only first tilde

--- server/src/YamlParserUtils.ts
+++ server/src/YamlParserUtils.ts
@@ -1,10 +1,14 @@
 import * as fs from 'node:fs';
 import { homedir } from 'node:os';
 import * as yaml from 'yaml';
+import path = require('node:path');
 
 export const YamlParserUtils = {
-  replaceTilde(path: string): string {
-    return path.replace('~', homedir());
+  replaceTilde(fsPath: string): string {
+    if (fsPath[0] === '~') {
+      return path.join(homedir(), fsPath.slice(1));
+    }
+    return fsPath;
   },
 
   parseYamlFile(filePath: string): unknown {
