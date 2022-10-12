b6f774acd5ea7f688d0c49974e4a1038d3e848a3
Pavel Grivachev <pavel@fivetran.com>
2022-10-13 00:04:16 +0200

    increase version in custom venv test

--- e2e/src/certain_version.spec.ts
+++ e2e/src/certain_version.spec.ts
@@ -3,18 +3,19 @@ import { activateAndWait, executeInstallLatestDbt, getCustomDocUri, getLatestDbt
 
 suite('Certain version', () => {
   const DOC_URI = getCustomDocUri('special-python-settings/models/version.sql');
+  const VENV_VERSION = '1.2.2';
 
   test('Should run project with dbt version specified for workspace', async () => {
     await activateAndWait(DOC_URI);
 
-    assertThat(getPreviewText(), '1.0.8');
+    assertThat(getPreviewText(), VENV_VERSION);
   }).timeout('100s');
 
   test('Should install latest dbt, restart language server and compile model with new dbt version', async () => {
     const latestVersion = getLatestDbtVersion();
     await activateAndWait(DOC_URI);
 
-    assertThat(getPreviewText(), '1.0.8');
+    assertThat(getPreviewText(), VENV_VERSION);
     await executeInstallLatestDbt();
 
     await waitPreviewModification();
