3389dd2ea51d7207f4e5487e8027ee6259a6774c
Pavel Grivachev <pavel@fivetran.com>
2022-10-27 11:33:05 +0200

    add profiles.yml language status item

--- client/src/status/LanguageStatusItems.ts
+++ client/src/status/LanguageStatusItems.ts
@@ -5,9 +5,11 @@ export class LanguageStatusItems {
   static readonly DBT_DEFAULT_TEXT = 'dbt';
   static readonly DBT_ADAPTERS_DEFAULT_TEXT = 'dbt Adapters';
   static readonly DBT_PACKAGES_DEFAULT_TEXT = 'dbt Packages';
+  static readonly PROFILES_YML_DEFAULT_TEXT = 'profiles.yml';
 
   python = new DbtWizardLanguageStatusItem('dbtWizardPython', LanguageStatusItems.PYTHON_DEFAULT_TEXT);
   dbt = new DbtWizardLanguageStatusItem('dbtWizardDbt', LanguageStatusItems.DBT_DEFAULT_TEXT);
   dbtAdapters = new DbtWizardLanguageStatusItem('dbtWizardDbtAdapters', LanguageStatusItems.DBT_ADAPTERS_DEFAULT_TEXT);
   dbtPackages = new DbtWizardLanguageStatusItem('dbtWizardDbtPackages', LanguageStatusItems.DBT_PACKAGES_DEFAULT_TEXT);
+  profilesYml = new DbtWizardLanguageStatusItem('dbtWizardProfilesYml', LanguageStatusItems.PROFILES_YML_DEFAULT_TEXT);
 }
