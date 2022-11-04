7c55969195765fef2d5e2a127c9d24a416de2ef9
Pavel Grivachev <pavel@fivetran.com>
2022-11-04 10:47:06 +0100

    add more snippets

--- server/src/SnippetsCompletionProvider.ts
+++ server/src/SnippetsCompletionProvider.ts
@@ -5,17 +5,33 @@ export class SnippetsCompletionProvider {
   static readonly SNIPPETS = [
     SnippetsCompletionProvider.createSnippet(
       'ref',
-      '{{ ref }}',
+      'dbt Ref',
       '1ref',
       "{{ ref('$0') }}",
       Command.create('triggerSuggest', 'editor.action.triggerSuggest'),
     ),
+
     SnippetsCompletionProvider.createSnippet(
       'config',
-      '{{ config }}',
+      'dbt Config',
       '1config',
       `{{${EOL}  config(${EOL}    materialized='$\{1|table,view,incremental,ephemeral|}'${EOL}  )${EOL}}}${EOL}`,
     ),
+
+    SnippetsCompletionProvider.createSnippet('if', 'dbt If', '1if', `{% if $\{1:condition} %}${EOL}  $\{2}${EOL}{% endif %}`),
+    SnippetsCompletionProvider.createSnippet(
+      'ifelse',
+      'dbt If-Else',
+      '1ifelse',
+      `{% if $\{1:condition} %}${EOL}  $\{2}${EOL}{% else %}${EOL}  ${EOL}{% endif %}`,
+    ),
+
+    SnippetsCompletionProvider.createSnippet('for', 'dbt For-In', '1for', `{% for $\{1:item} in $\{2:array} %}${EOL}  $\{3}${EOL}{% endfor %}`),
+
+    SnippetsCompletionProvider.createSnippet('macro', 'dbt Macro', '1macro', `{% macro $\{1:name}($\{2:args}) %}${EOL}  $\{3}${EOL}{% endmacro %}`),
+
+    SnippetsCompletionProvider.createSnippet('set', 'dbt Set', '1set', '{% set ${1:var_name} = ${2:value} %}'),
+    SnippetsCompletionProvider.createSnippet('setblock', 'dbt Set-Block', '1setblock', `{% set $\{1:name} %}${EOL}  $\{2}${EOL}{% endset %}`),
   ];
 
   static createSnippet(label: string, detail: string, sortText: string, insertText: string, command?: Command): CompletionItem {
