92c6cf51065a2d44c243acc0c950e7ed20852672
Pavel Grivachev <pavel@fivetran.com>
2022-12-06 15:33:37 +0100

    fix project path

--- e2e/src/runners/runFtTest.ts
+++ e2e/src/runners/runFtTest.ts
@@ -4,7 +4,7 @@ import { installVsCodeAndRunTests } from './runTestUtils';
 // Expected parameter: path to the folder with the extension package.json
 async function main(): Promise<void> {
   console.log('Running dbt_ft tests');
-  await installVsCodeAndRunTests('index_dbt_ft', path.resolve(__dirname, '../../../analytics/dbt_ft_prod'));
+  await installVsCodeAndRunTests('index_dbt_ft', path.resolve(__dirname, '../../../analytics/dbt_ft_prod.code-workspace'));
 }
 
 main().catch(e => console.error(e));
