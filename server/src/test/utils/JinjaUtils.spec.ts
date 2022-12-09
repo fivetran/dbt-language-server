c66174652edb994432e3125b83d24c5698403eb7
Pavel Grivachev <pavel@fivetran.com>
2022-12-09 14:27:33 +0100

    support default value

--- server/src/test/utils/JinjaUtils.spec.ts
+++ server/src/test/utils/JinjaUtils.spec.ts
@@ -8,13 +8,20 @@ describe('JinjaUtils', () => {
 
   it('evalJinjaEnvVar should not replace value if it is found in environment', () => {
     process.env['TEST2'] = 'test2_value';
+    process.env['TEST3'] = 'test3_value';
     evalJinjaEnvVarShouldReturnEnvironmentValue("{{env_var('TEST2')}}", 'test2_value');
+    evalJinjaEnvVarShouldReturnEnvironmentValue("{{env_var('TEST2')}}_{{env_var('TEST3')}}", 'test2_value_test3_value');
     evalJinjaEnvVarShouldReturnEnvironmentValue('{{env_var("TEST2")}}', 'test2_value');
     evalJinjaEnvVarShouldReturnEnvironmentValue('{{ env_var("TEST2") }}', 'test2_value');
     evalJinjaEnvVarShouldReturnEnvironmentValue('{{ env_var("TEST2")   }}', 'test2_value');
     evalJinjaEnvVarShouldReturnEnvironmentValue('{{   env_var("TEST2")   }}', 'test2_value');
     evalJinjaEnvVarShouldReturnEnvironmentValue('{{   env_var("TEST2")   }}_b', 'test2_value_b');
     evalJinjaEnvVarShouldReturnEnvironmentValue('a_{{   env_var("TEST2")   }}_b', 'a_test2_value_b');
+
+    evalJinjaEnvVarShouldReturnEnvironmentValue('{{ env_var("TEST1", "default") }}', 'default');
+    evalJinjaEnvVarShouldReturnEnvironmentValue('{{ env_var("TEST1", "default") }}_{{ env_var("TEST1", "default") }}', 'default_default');
+    evalJinjaEnvVarShouldReturnEnvironmentValue("{{ env_var('TEST1', 'default') }}", 'default');
+    evalJinjaEnvVarShouldReturnEnvironmentValue('{{env_var("TEST1",   "default")  }}', 'default');
   });
 });
 
--- server/src/utils/JinjaUtils.ts
+++ server/src/utils/JinjaUtils.ts
@@ -1,13 +1,22 @@
 export function evalJinjaEnvVar(text: string): string {
-  const regex = /{{\s*env_var\(['"](.*)['"]\)\s*}}/g;
+  const regex = /{{\s*env_var\((['"].*?['"])\)\s*}}/g;
   let resultText = text;
   const matches = resultText.match(regex);
   if (matches) {
     matches.forEach(match => {
-      const envVar = match.replace(regex, '$1');
-      const envVarValue = process.env[envVar];
-      if (envVarValue) {
-        resultText = resultText.replace(match, envVarValue);
+      const params = match
+        .replace(regex, '$1')
+        .split(',')
+        .map(p => p.trim())
+        .map(p => p.replace(/^['"]/, '').replace(/['"]$/, ''));
+
+      if (params.length > 0) {
+        const envVarValue = process.env[params[0]];
+        if (envVarValue) {
+          resultText = resultText.replace(match, envVarValue);
+        } else if (params.length > 1) {
+          resultText = resultText.replace(match, params[1]);
+        }
       }
     });
   }
