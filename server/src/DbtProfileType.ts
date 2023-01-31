9d762ebc151ac194d3fbd20124b14b521e95d4b7
Pavel Grivachev <pavel@fivetran.com>
2023-01-31 15:09:13 +0100

    rename ServiceAccountJsonProfile to BigQueryServiceAccountJsonProfile

--- server/src/DbtProfileType.ts
+++ server/src/DbtProfileType.ts
@@ -1,15 +1,15 @@
 import { BigQueryOAuthProfile } from './bigquery/BigQueryOAuthProfile';
 import { BigQueryOAuthTokenBasedProfile } from './bigquery/BigQueryOAuthTokenBasedProfile';
-import { ServiceAccountJsonProfile } from './bigquery/ServiceAccountJsonProfile';
-import { ServiceAccountProfile } from './bigquery/ServiceAccountProfile';
+import { BigQueryServiceAccountJsonProfile } from './bigquery/BigQueryServiceAccountJsonProfile';
+import { BigQueryServiceAccountProfile } from './bigquery/ServiceAccountProfile';
 import { DbtProfile, DbtProfileType } from './DbtProfile';
 import { SnowflakeUserPassProfile } from './snowflake/SnowflakeUserPassProfile';
 
 export const BIG_QUERY_PROFILES = new Map<string, () => DbtProfile>([
   ['oauth', (): DbtProfile => new BigQueryOAuthProfile()],
   ['oauth-secrets', (): DbtProfile => new BigQueryOAuthTokenBasedProfile()],
-  ['service-account', (): DbtProfile => new ServiceAccountProfile()],
-  ['service-account-json', (): DbtProfile => new ServiceAccountJsonProfile()],
+  ['service-account', (): DbtProfile => new BigQueryServiceAccountProfile()],
+  ['service-account-json', (): DbtProfile => new BigQueryServiceAccountJsonProfile()],
 ]);
 
 export const SNOWFLAKE_PROFILES = new Map<string, () => DbtProfile>([['user-password', (): DbtProfile => new SnowflakeUserPassProfile()]]);
