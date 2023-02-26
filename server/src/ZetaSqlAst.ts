e3492ec87680ecc2396edeaa344580d5464c296f
Pavel Grivachev <pavel@fivetran.com>
2023-02-26 23:24:06 +0100

    handle aggregate scan separately

--- server/src/ZetaSqlAst.ts
+++ server/src/ZetaSqlAst.ts
@@ -2,7 +2,6 @@
 
 import { TypeKind } from '@fivetrandevelopers/zetasql';
 import { Type } from '@fivetrandevelopers/zetasql/lib/Type';
-import { AnyResolvedAggregateScanBaseProto__Output } from '@fivetrandevelopers/zetasql/lib/types/zetasql/AnyResolvedAggregateScanBaseProto';
 import { AnyResolvedScanProto__Output } from '@fivetrandevelopers/zetasql/lib/types/zetasql/AnyResolvedScanProto';
 import { AnalyzeResponse__Output } from '@fivetrandevelopers/zetasql/lib/types/zetasql/local_service/AnalyzeResponse';
 import { ParseLocationRangeProto, ParseLocationRangeProto__Output } from '@fivetrandevelopers/zetasql/lib/types/zetasql/ParseLocationRangeProto';
@@ -282,66 +281,69 @@ export class ZetaSqlAst {
   }
 
   getWithSubqueryInfo(node: AnyResolvedScanProto__Output | null, completionInfo: CompletionInfo, withQueryName: string): void {
-    if (node) {
-      const subqueryNode = node[node.node];
-      if (subqueryNode && 'parent' in subqueryNode) {
-        const scanProto = subqueryNode.parent;
-        let activeWith: WithSubqueryInfo | undefined = undefined;
-        if (scanProto) {
-          activeWith = completionInfo.withSubqueries.get(withQueryName);
-          if (!activeWith) {
-            activeWith = {
-              columns: [],
-              parseLocationRange: scanProto.parent?.parseLocationRange ?? undefined,
-            };
-            completionInfo.withSubqueries.set(withQueryName, activeWith);
+    if (!node) {
+      return;
+    }
+    const subqueryNode = node[node.node];
+    if (subqueryNode && 'parent' in subqueryNode) {
+      const scanProto = subqueryNode.parent;
+      if (scanProto) {
+        let activeWith = completionInfo.withSubqueries.get(withQueryName);
+        if (!activeWith) {
+          activeWith = {
+            columns: [],
+            parseLocationRange: scanProto.parent?.parseLocationRange ?? undefined,
+          };
+          completionInfo.withSubqueries.set(withQueryName, activeWith);
+        }
+        scanProto.columnList.forEach(c => {
+          if (c.name) {
+            activeWith?.columns.push({
+              name: c.name,
+              type: c.type?.typeKind ? Type.TYPE_KIND_NAMES[c.type.typeKind as TypeKind] : undefined,
+              fromTable: c.tableName,
+            });
           }
-          scanProto.columnList.forEach(c => {
-            if (c.name) {
-              activeWith?.columns.push({
-                name: c.name,
-                type: c.type?.typeKind ? Type.TYPE_KIND_NAMES[c.type.typeKind as TypeKind] : undefined,
-                fromTable: c.tableName,
+        });
+      }
+      if ('inputScan' in subqueryNode) {
+        const { inputScan } = subqueryNode;
+        if (inputScan) {
+          const inputScanNode = inputScan[inputScan.node];
+          if (inputScanNode && 'withQueryName' in inputScanNode) {
+            const existingWith = completionInfo.withSubqueries.get(inputScanNode.withQueryName);
+            if (existingWith) {
+              inputScanNode.parent?.columnList.forEach((c, i) => {
+                if (c.name !== existingWith.columns[i].name) {
+                  existingWith.columns[i].name = c.name;
+                }
               });
             }
-          });
+          } else {
+            this.getWithSubqueryInfo(inputScan, completionInfo, withQueryName);
+          }
         }
-        if ('inputScan' in subqueryNode && activeWith) {
-          const { inputScan } = subqueryNode;
-          if (inputScan) {
-            const inputScanNode = inputScan[inputScan.node];
-            if (inputScanNode && 'withQueryName' in inputScanNode) {
-              const existingWith = completionInfo.withSubqueries.get(inputScanNode.withQueryName);
-              if (existingWith) {
-                inputScanNode.parent?.columnList.forEach((c, i) => {
-                  if (c.name !== existingWith.columns[i].name) {
-                    existingWith.columns[i].name = c.name;
-                  }
-                });
-              }
-            } else if (inputScan.node === 'resolvedAggregateScanBaseNode') {
-              const aggregateScanBase = inputScanNode as AnyResolvedAggregateScanBaseProto__Output;
-              // TODO: check 'resolvedAnonymizedAggregateScanNode'
-              if (aggregateScanBase.node === 'resolvedAggregateScanNode') {
-                const aggregateScan = aggregateScanBase[aggregateScanBase.node];
-                aggregateScan?.parent?.groupByList.forEach(g => {
-                  if (g.expr?.node === 'resolvedColumnRefNode') {
-                    const columnRef = g.expr[g.expr.node];
-                    const column = columnRef?.column;
-                    const groupedColumn = g.column;
-                    if (column && groupedColumn) {
-                      const foundColumn = activeWith?.columns.find(c => c.name === groupedColumn.name);
-                      if (foundColumn) {
-                        foundColumn.fromTable = column.tableName;
-                        foundColumn.name = column.name;
-                      }
-                    }
-                  }
-                });
+      }
+    } else if (node.node === 'resolvedAggregateScanBaseNode') {
+      const aggregateScanBase = node[node.node];
+      // TODO: check 'resolvedAnonymizedAggregateScanNode'
+      if (aggregateScanBase && aggregateScanBase.node === 'resolvedAggregateScanNode') {
+        const aggregateScan = aggregateScanBase[aggregateScanBase.node];
+        aggregateScan?.parent?.groupByList.forEach(g => {
+          if (g.expr?.node === 'resolvedColumnRefNode') {
+            const columnRef = g.expr[g.expr.node];
+            const column = columnRef?.column;
+            const groupedColumn = g.column;
+            const foundWith = completionInfo.withSubqueries.get(withQueryName);
+            if (column && groupedColumn && foundWith) {
+              const foundColumn = foundWith.columns.find(c => c.name === groupedColumn.name);
+              if (foundColumn) {
+                foundColumn.fromTable = column.tableName;
+                foundColumn.name = column.name;
               }
             }
           }
-        }
+        });
       }
     }
   }
