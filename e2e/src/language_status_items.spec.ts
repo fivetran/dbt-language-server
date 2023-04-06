acc8d7761297dccacc5b43b1fc4c386f0921cf85
Pavel Grivachev <pavel@fivetran.com>
2023-04-06 09:10:49 +0200

    add asserts

--- e2e/src/language_status_items.spec.ts
+++ e2e/src/language_status_items.spec.ts
@@ -1,4 +1,4 @@
-import { assertThat, startsWith } from 'hamjest';
+import { assertThat, containsString, startsWith } from 'hamjest';
 import { LanguageStatusSeverity } from 'vscode';
 import { TEST_FIXTURE_PATH, createAndOpenTempModel, executeCreateFile, getLanguageStatusItems, sleep } from './helper';
 
@@ -16,26 +16,32 @@ suite('Language status items', () => {
 function assertProjectLanguageStatusItems(): void {
   const items = getLanguageStatusItems();
   assertThat(items.activeDbtProject.busy, false);
-  assertThat(items.activeDbtProject.text, 'dbt project');
+  assertThat(items.activeDbtProject.text, 'Current project');
   assertThat(items.activeDbtProject.detail, TEST_FIXTURE_PATH);
 
   assertThat(items.python.busy, false);
 
   assertThat(items.dbt.busy, false);
-  assertThat(items.dbt.text, startsWith('dbt '));
+  assertThat(items.dbt.text, startsWith('dbt Core'));
+  assertThat(items.dbt.command?.title, 'Install different version');
 
   assertThat(items.dbtAdapters.busy, false);
+  assertThat(items.dbtAdapters.text, containsString('bigquery'));
   assertThat(items.dbtAdapters.detail, 'installed dbt adapters');
   assertThat(items.dbtAdapters.severity, LanguageStatusSeverity.Information);
+  assertThat(items.dbtAdapters.command?.title, 'Install different adapter');
 
   assertThat(items.dbtPackages.busy, false);
   assertThat(items.dbtPackages.text, 'No packages.yml');
   assertThat(items.dbtPackages.detail, '');
   assertThat(items.dbtPackages.severity, LanguageStatusSeverity.Information);
-  assertThat(items.dbtPackages.command?.title, 'Install dbt Packages');
+  assertThat(items.dbtPackages.command?.title, 'Install dbt packages');
 
   assertThat(items.profilesYml.busy, false);
-  assertThat(items.profilesYml.text, 'profiles.yml');
+  assertThat(items.profilesYml.text, 'e2e-test-project');
+  assertThat(items.profilesYml.detail, 'Current profile');
+  assertThat(items.profilesYml.severity, LanguageStatusSeverity.Information);
+  assertThat(items.profilesYml.command?.title, 'Change target credentials');
 }
 
 function assertNoProjectLanguageStatusItems(): void {
@@ -47,7 +53,12 @@ function assertNoProjectLanguageStatusItems(): void {
   assertThat(items.python.busy, false);
 
   assertThat(items.dbt.busy, false);
+  assertThat(items.dbt.text, startsWith('dbt Core'));
+  assertThat(items.dbt.command?.title, 'Install different version');
 
   assertThat(items.dbtAdapters.busy, false);
+  assertThat(items.dbtAdapters.text, containsString('bigquery'));
   assertThat(items.dbtAdapters.detail, 'installed dbt adapters');
+  assertThat(items.dbtAdapters.severity, LanguageStatusSeverity.Information);
+  assertThat(items.dbtAdapters.command?.title, 'Install different adapter');
 }
