106d8f445a6a52b6de9273a8af5d0195fef76115
Pavel Grivachev <pavel@fivetran.com>
2023-05-01 20:53:09 +0200

    comment test

--- e2e/projects/test-fixture/models/compare_dates.sql
+++ e2e/projects/test-fixture/models/compare_dates.sql
@@ -1,3 +1,4 @@
-select new_date
-from {{ ref('my_new_project', 'table_does_not_exist') }}
-where new_date = date_sub(current_date(), interval 1 day)
+-- select new_date
+-- TODO: Uncomment and add { later (doesn't work in 1.5.0)
+-- from { ref('my_new_project', 'table_does_not_exist') }}
+-- where new_date = date_sub(current_date(), interval 1 day)
