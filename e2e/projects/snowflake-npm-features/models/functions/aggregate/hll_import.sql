df5c11a4108d5f15ceeb15023af27269a0049b5f
Pavel Grivachev <pavel@fivetran.com>
2023-05-21 11:49:22 +0200

    fix queries

--- e2e/projects/snowflake-npm-features/models/functions/aggregate/hll_import.sql
+++ e2e/projects/snowflake-npm-features/models/functions/aggregate/hll_import.sql
@@ -1,28 +1,30 @@
-WITH sales_data AS (
-    SELECT 1 AS customer_id
-    UNION ALL
-    SELECT 1
-    UNION ALL
-    SELECT 2
-    UNION ALL
-    SELECT 3
-    UNION ALL
-    SELECT 3
-    UNION ALL
-    SELECT 4
-    UNION ALL
-    SELECT 4
-    UNION ALL
-    SELECT 5
-    UNION ALL
-    SELECT 6
+WITH source_data AS (
+  SELECT 'A' as letter UNION ALL
+  SELECT 'B' UNION ALL
+  SELECT 'A' UNION ALL
+  SELECT 'C' UNION ALL
+  SELECT 'B' UNION ALL
+  SELECT 'D' UNION ALL
+  SELECT 'E' UNION ALL
+  SELECT 'F' UNION ALL
+  SELECT 'G' UNION ALL
+  SELECT 'H'
 ),
+
+hll_accumulated AS (
+  SELECT HLL_ACCUMULATE(letter) as hll
+  FROM source_data
+),
+
 hll_combined AS (
-    SELECT HLL_COMBINE(HLL(customer_id)) AS hll_result
-    FROM sales_data
+  SELECT HLL_COMBINE(hll) as hll_combined
+  FROM hll_accumulated
 ),
+
 hll_exported AS (
-    SELECT HLL_EXPORT(hll_result) AS hll_exported
-    FROM hll_combined
+  SELECT HLL_EXPORT(hll_combined) as hll_exported
+  FROM hll_combined
 )
-SELECT HLL_ESTIMATE(HLL_IMPORT(hll_exported)) AS unique_customers FROM hll_exported;
+
+SELECT HLL_IMPORT(hll_exported) as hll_imported
+FROM hll_exported;
