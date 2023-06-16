bdf886de5bfa6a1ade787d147afcb4f3600846d3
Pavel Grivachev <pavel@fivetran.com>
2023-06-16 11:19:13 +0200

    fix test

--- e2e/projects/snowflake-npm-features/models/data_types/array.sql
+++ e2e/projects/snowflake-npm-features/models/data_types/array.sql
@@ -1,1 +1,5 @@
-SELECT CAST('1234567' AS ARRAY);
+select
+  array_construct(1, 2, 3) as num_array,
+  array_construct('a', 'b', 'c') as str_array,
+  array_construct(null, 'hello', 3.5, 4, 5) mixed_array,
+  array_construct(current_timestamp(), current_timestamp()) as timestamp_array
