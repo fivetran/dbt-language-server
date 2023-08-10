73ff8f2b4c2af6b9ccf5ad7df91634a695fa225b
Pavel Grivachev <pavel@fivetran.com>
2023-08-10 20:31:49 +0200

    add snowflake support for script

--- server/python/script.py
+++ server/python/script.py
@@ -1,12 +1,10 @@
 import http
-import os
 import sys
 from typing import List
 from dbt.adapters.factory import FACTORY
 from dbt.contracts.connection import AdapterRequiredConfig
-from dbt.adapters.bigquery import BigQueryColumn
-from dbt.adapters.bigquery.relation import BigQueryRelation
-import google.cloud.exceptions
+from dbt.adapters.base import Column as BaseColumn
+from dbt.adapters.base.relation import BaseRelation
 import json
 
 # Expected arguments for this script: 
@@ -34,7 +32,9 @@ if sys.argv[1] == "--version":
     dbt_command(["--version"])
 else:
     port = sys.argv[1]
-    def new_get_columns_in_relation(self, relation: BigQueryRelation) -> List[BigQueryColumn]:
+    global_old_get_columns_in_relation = None
+    def new_get_columns_in_relation(self, relation: BaseRelation) -> List[BaseColumn]:
+        global global_old_get_columns_in_relation
         db = relation.path.database if relation.path.database != None else "None"
         schema = relation.path.schema if relation.path.schema != None else "None"
         table = relation.path.identifier if relation.path.identifier != None else "None"
@@ -46,26 +46,20 @@ else:
         result = response.read().decode();
         if result not in ["SOURCE", "NOT_FOUND"]:
             data = json.loads(result)
-            bigquery_columns = [BigQueryColumn(column[0], BigQueryColumn.translate_type(column[1])) for column in data]
-
+            bigquery_columns = [self.Column.create(column[0], column[1]) for column in data]
             return bigquery_columns
-            
-        try:
-            table_from_bq = self.connections.get_bq_table(
-                database=relation.database, schema=relation.schema, identifier=relation.identifier
-            )
-            return self._get_dbt_columns_from_bq_table(table_from_bq)
 
-        except (ValueError, google.cloud.exceptions.NotFound) as e:
-            return []
+        return global_old_get_columns_in_relation(relation)
 
     old_register_adapter = FACTORY.register_adapter.__get__(FACTORY)
 
     def new_register_adapter(self, config: AdapterRequiredConfig) -> None:
+        global global_old_get_columns_in_relation
         old_register_adapter(config)
         credentials_type = config.credentials.type
         if credentials_type in ["bigquery", "snowflake"]:
             adapter = self.adapters[credentials_type]
+            global_old_get_columns_in_relation = adapter.get_columns_in_relation
             adapter.get_columns_in_relation = new_get_columns_in_relation.__get__(adapter)
 
     FACTORY.register_adapter = new_register_adapter.__get__(FACTORY)
