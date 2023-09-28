38cf9b35148a0e319674c0f55370c179bbbbee1b
Pavel Grivachev <pavel@fivetran.com>
2023-09-28 15:55:36 +0200

    use executable in tests

--- server/src/test/lsp_tests/lsp.spec.ts
+++ server/src/test/lsp_tests/lsp.spec.ts
@@ -37,8 +37,8 @@ describe('lsp tests', () => {
   before(async function b() {
     this.timeout('10s');
 
-    const args = ['-r', 'ts-node/register', path.resolve('server', 'out', 'server.js'), '--stdio'];
-    const child = spawn('node', args, { cwd: path.resolve(PROJECT_PATH) });
+    const executablePath = path.resolve('server', 'bin', 'dbt-language-server');
+    const child = spawn(executablePath, ['--stdio'], { cwd: path.resolve(PROJECT_PATH) });
     child.on('exit', code => {
       assertThat(code, 0);
     });
